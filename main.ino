b3ee88647349af5e9428e61b6d81cf350f52803e
Julius Kamp <juliuskamp2000@gmail.com>
2021-11-14 23:20:31 +0100

    specify fastLED parameters

--- main.ino
+++ main.ino
@@ -1,5 +1,12 @@
 #include <FastLED.h>
 
+#define LED_PIN 21
+#define NUM_LEDS 60
+#define BRIGHTNESS 32
+#define LED_TYPE WS2812B
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+
 void setup()
 {
 	
