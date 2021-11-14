70b181b2352e103a222f0dc961345d1b4a264d15
Julius Kamp <juliuskamp2000@gmail.com>
2021-11-14 23:42:32 +0100

    add function to remap the leds in a order representing the physical clock

--- main.ino
+++ main.ino
@@ -5,14 +5,33 @@
 #define BRIGHTNESS 32
 #define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
+CRGB setupLeds[NUM_LEDS];
 CRGB leds[NUM_LEDS];
 
 void setup()
 {
-	
+	FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(setupLeds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+	FastLED.setBrightness(BRIGHTNESS);
+
+    remapLeds(setupLeds, leds);
+
+    leds[59] = CRGB::White;
+	leds[14] = CRGB::White;
+	leds[29] = CRGB::White;
+	leds[44] = CRGB::White;
+	FastLED.show();
 }
 
 void loop()
 {
 	
+}
+
+void remapLeds(CRGB* setupLeds, CRGB* remapedLeds) {
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+        int currentLed = (i <= 29) ? (i - 30) : (i + 30);
+        leds[i] = setupLeds[currentLed];
+    }
+    
 }
