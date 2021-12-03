b59aee259150a69577beb10969b3746e5e4af646
Julius Kamp <juliuskamp2000@gmail.com>
2021-12-03 15:33:44 +0100

    change remapping of leds to be done with a array

--- main.ino
+++ main.ino
@@ -14,6 +14,8 @@ const int   daylightOffset_sec = 3600;
 #define COLOR_ORDER GRB
 CRGB leds[NUM_LEDS];
 
+int ledLookup[60] = {30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31};
+
 void setup()
 {
     WiFiManager wifiManager;
@@ -29,14 +31,4 @@ void setup()
 void loop()
 {
 	
-}
-
-void remapLeds(CRGB* leds) {
-    CRGB tempLeds[NUM_LEDS];
-    for (int i = 0; i < NUM_LEDS; i++)
-    {
-        int currentLed = (i <= 29) ? (i - 30) : (i + 30);
-        tempLeds[i] = leds[currentLed];
-    }
-    leds = tempLeds;
 }
